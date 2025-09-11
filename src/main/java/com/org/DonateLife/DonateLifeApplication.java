d8e257dd3c76b57719f6a8a065dbe2463581b0ee
AsitSwain2002 <147136555+AsitSwain2002@users.noreply.github.com>
2025-09-11 12:52:38 +0530

    first commit

---
+++ src/main/java/com/org/DonateLife/DonateLifeApplication.java
@@ -0,0 +1,13 @@
+package com.org.DonateLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonateLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonateLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/DonateLife/DonateLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.DonateLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonateLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
