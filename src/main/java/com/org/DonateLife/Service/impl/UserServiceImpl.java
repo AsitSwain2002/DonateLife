119e472767344c6b7083c7eb9de582dc851f6581
AsitSwain2002 <147136555+AsitSwain2002@users.noreply.github.com>
2025-09-13 11:07:28 +0530

    Added User Update Module

--- src/main/java/com/org/DonateLife/Service/impl/UserServiceImpl.java
+++ src/main/java/com/org/DonateLife/Service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.org.DonateLife.Service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,12 +10,15 @@ import org.springframework.util.ObjectUtils;
 
 import com.org.DonateLife.Dto.AddressDto;
 import com.org.DonateLife.Dto.UsersDto;
+import com.org.DonateLife.Dto.UsersDto.RolesDto;
 import com.org.DonateLife.Entity.Address;
 import com.org.DonateLife.Entity.BloodGroup;
+import com.org.DonateLife.Entity.Roles;
 import com.org.DonateLife.Entity.Users;
 import com.org.DonateLife.ExceptionHandling.ResourceNotFound;
 import com.org.DonateLife.Repo.AddressRepo;
 import com.org.DonateLife.Repo.BloodGroupRepo;
+import com.org.DonateLife.Repo.RolesRepo;
 import com.org.DonateLife.Repo.UserRepo;
 import com.org.DonateLife.Service.UserService;
 
@@ -27,6 +33,9 @@ public class UserServiceImpl implements UserService {
 	private AddressRepo addressRepo;
 
 	@Autowired
+	private RolesRepo rolesRepo;
+
+	@Autowired
 	private ModelMapper mapper;
 
 	@Override
@@ -34,20 +43,58 @@ public class UserServiceImpl implements UserService {
 		// validation apply here
 
 		Users user = mapper.map(userDto, Users.class);
+		// Blood Group set here
+		setBloodGroup(user);
+
+		// address save here
+		setAddress(user);
+
+		// role set Here
+		setRole(userDto, user);
+
+		// update logic
+
+		if (userDto.getId() != 0) {
+			updateUser(user);
+		}
+		Users save = userRepo.save(user);
+
+		if (!ObjectUtils.isEmpty(save)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void updateUser(Users user) {
+		Users dbUser = userRepo.findById(user.getId()).orElseThrow(() -> new ResourceNotFound("User Id Not Found"));
+
+	}
+
+	private void setAddress(Users user) {
+		Address address = addressRepo.save(user.getAddress());
+		user.setAddress(address);
+
+	}
+
+	private void setBloodGroup(Users user) {
 		BloodGroup bloodGroup = bloodGroupRepo.findById(user.getBloodGroup().getId())
 				.orElseThrow(() -> new ResourceNotFound("Blood Group Id Not found"));
 		user.setBloodGroup(bloodGroup);
 
-		// address save here
-		Address address = addressRepo.save(user.getAddress());
-		user.setAddress(address);
+	}
 
-		Users save = userRepo.save(user);
+	private void setRole(UsersDto userDto, Users user) {
+		List<RolesDto> roles = userDto.getRoles();
+		List<Integer> list = roles.stream().map((e) -> e.getId()).toList();
+		List<Roles> findAllById = rolesRepo.findAllById(list);
+		user.setRoles(findAllById);
 
-		if (!ObjectUtils.isEmpty(save)) {
-			return true; 
-		}
-		return false;
+	}
+
+	@Override
+	public UsersDto findById(Integer id) {
+		Users user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFound("User Id Not Found"));
+		return mapper.map(user, UsersDto.class);
 	}
 
 }
